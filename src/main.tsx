5043322e3a136c8cc722326bc077baa5144c25f3
cheng_zhuo <cz2545481217@outlook.com>
2025-09-01 13:29:41 +0800

    init project

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    my first react app
+  </React.StrictMode>,
+)
+
+// Use contextBridge
+window.ipcRenderer.on('main-process-message', (_event, message) => {
+  console.log(message)
+})
